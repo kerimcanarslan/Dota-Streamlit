f295b66334d962a809b0b61da97050fd29190e0c
Kerimcan Arslan <115086928+kerimcanarslan@users.noreply.github.com>
2023-01-19 12:35:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class ColumnSelector(BaseEstimator, TransformerMixin):
 def Dota():
 
     
-    st.markdown(f'<h1 <p><font face="tahoma" size="130" color="white"><b>DotA-2 Win Simulation</b></font></p> </h1>', unsafe_allow_html=True)
+    st.markdown(f'<h1 <p><font face="tahoma" size="130" color="white"><b>    DotA-2 Win Simulation</b></font></p> </h1>', unsafe_allow_html=True)
    
 
     dsg = st.sidebar.selectbox('**Go to**', ["Intro", "Predict Probability"])
