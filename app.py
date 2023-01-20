d01deb7736c4e82cc5125dff89ca396e04507cdc
Kerimcan Arslan <115086928+kerimcanarslan@users.noreply.github.com>
2023-01-20 21:54:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -825,10 +825,10 @@ def NLP():
         prediction_count = predict(inputs)
         if prediction_count == 1:
             st.write("Prediction: ", 'CONGRATULATIONS! I THINK YOU WON!')
-            st.write("Prediction: ", 'Oyuncu maç esnasında negatif ve argo kelime kullanmadığı için maçı kazanıyor olabilir')
+            st.markdown(f'<h1 <p><font face="tahoma" size="8" color="crimson "><b>Oyuncu maç esnasında negatif ve argo kelime kullanmadığı için maçı kazanıyor olabilir</b></font></p> </h1>', unsafe_allow_html=True)
         else:
             st.write("Prediction: ", 'PLEASE DO NOT USE SLANG WORDS!')
-            st.markdown(f'<h1 <p><font face="tahoma" size="4" color="maroon "><b>Oyuncu maç esnasında negatif ve argo kelime kullandığı için maçı kaybediyor olabilir</b></font></p> </h1>', unsafe_allow_html=True)
+            st.markdown(f'<h1 <p><font face="tahoma" size="8" color="crimson "><b>Oyuncu maç esnasında negatif ve argo kelime kullandığı için maçı kaybediyor olabilir</b></font></p> </h1>', unsafe_allow_html=True)
             
             
 
